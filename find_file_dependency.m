8de86c7aafb9b2e84bc4a6fff399be4375f01bdc
Jiaweixu-pitt <60493789+Jiaweixu-pitt@users.noreply.github.com>
2020-01-30 21:34:50 -0500

    Update find_file_dependency.m

--- find_file_dependency.m
+++ find_file_dependency.m
@@ -1,6 +1,5 @@
 % find file dependency
 fList = matlab.codetools.requiredFilesAndProducts('IP_EF_SRMSE.m');
-
 filePh = fopen('dependency.txt','w');
 fprintf(filePh,'%s\n',fList{:});
 fclose(filePh);
