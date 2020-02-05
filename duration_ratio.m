de216c9b3eba5b80513b5f8fa68698be7e63fdc9
jgrantbalt <60588292+jgrantbalt@users.noreply.github.com>
2020-02-05 13:51:43 -0500

    editing

--- duration_ratio.m
+++ duration_ratio.m
@@ -1,6 +1,4 @@
 function [ dr ] =  duration_ratio(report1, report2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
 % Duration rate: dr =  abs(d1 - d2)/max(d1,d2)
 d1 = report1(2) - report1(1);
 d2 = report2(2) - report2(1);
