03a893e18053a0d83be96337ff63138a79fe2089
jgrantbalt <60588292+jgrantbalt@users.noreply.github.com>
2020-02-05 13:58:49 -0500

    editing

--- proximity_ratio_new_2.m
+++ proximity_ratio_new_2.m
@@ -1,8 +1,5 @@
 function [ pr ] =  proximity_ratio_new_2(report1, report2,period)
-% new pr version: when two reports close to each other, pr-->1
-%   Detailed explanation goes here
-
-
+% new pr version
 % proximity ratio pr =  abs(center1 - center2)/total_data_duration
 center1 = (report1(2) + report1(1))/2;
 center2 = (report2(2) + report2(1))/2;
@@ -20,7 +17,7 @@ if (period == 0)
     end
     pr =  1 - abs(center1 - center2)/two_reports_duration;
 else
-    % pri,j= 1?abs(sin(disti,j×?/p))  periodic example
+    % pri,j= 1?abs(sin(disti,jÃ—?/p))  periodic example
     pr = 1 - abs(sin(3.1415926*abs(center1-center2)/period));
 end
 
