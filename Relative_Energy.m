6e37aaecd59b46c2c2c5e66d39e8b9684cc6c211
jgrantbalt <60588292+jgrantbalt@users.noreply.github.com>
2020-02-05 13:44:51 -0500

    editing

--- Relative_Energy.m
+++ Relative_Energy.m
@@ -1,12 +1,11 @@
 function [ Energy_Relative ] = Relative_Energy( userSize, Energy )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%Calculate Relative Energy
 N = userSize;
 E_Max = max(Energy);
 Energy_Relative = zeros(N,1);
 
 if (E_Max == 0)
-    Energy_Relative = Energy; % all isolated node, not diveded by 0
+    Energy_Relative = Energy; % isolated node, not divided by 0
 else
     for i=1:N
         Energy_Relative(i) = Energy(i)/E_Max;
